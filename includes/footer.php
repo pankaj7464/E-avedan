8784b1804f28ee735c583e88877eaae993626f7c
pankaj7464 <pkumar44676@gmail.com>
2023-07-15 10:12:53 +0530

    initial stage

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | E-avedan | Developed By <a href="https://pankajkumar9.netlify.app/">Pankaj Kumar </a></p>
+        </div>
+</footer>
